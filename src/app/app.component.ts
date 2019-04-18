4976c5629625edeb7e563da129465ea312c18e7a
gust44444 <gustavoheidemann1@gmail.com>
2019-04-17 22:06:27 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nome = 'Gustavo';
+
+  categorias = [
+    {id:"1", nome:"categoria 1"},
+    {id:"2", nome:"categoria 2"},
+    {id:"3", nome:"categoria 3"},
+    {id:"4", nome:"categoria 4"},
+    {id:"5", nome:"categoria 5"},
+    {id:"6", nome:"categoria 6"},
+    {id:"7", nome:"categoria 7"},
+    {id:"8", nome:"categoria 8"},
+    {id:"9", nome:"categoria 9"},
+    {id:"10", nome:"categoria 10"}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+
+import {ButtonModule} from 'primeng/button';
+
+import {TableModule} from 'primeng/table';
+
+import {TooltipModule} from 'primeng/tooltip';
+
+import {PaginatorModule} from 'primeng/paginator';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
